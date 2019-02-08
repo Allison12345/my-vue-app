2daf21704059876bd26b034689bed21d53a8a9d0
Allison12345 <2562392373@qq.com>
2019-02-08 20:22:42 +0800

    my-vue-app

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+Vue.use(Vuex) 
+
 Vue.use(Vuex)
+
+export const NEWS_LIST = 'NEWS_LIST'
+export const SAVE_NEWS_LIST = 'SAVE_NEWS_LIST'
+export const NEWS_DETAIL = 'NEWS_DETAIL'
+export const SAVE_NEWS_DETAIL = 'SAVE_NEWS_DETAIL'
+
+const state = {
+  [NEWS_LIST]: [],
+  [NEWS_DETAIL]: {}
+}
+
+const getters = {
+  [NEWS_LIST]: state => state[NEWS_LIST],
+  [NEWS_DETAIL]: state => state[NEWS_DETAIL]
+}
+
+const mutations = {
+  [SAVE_NEWS_LIST](state, list) {
+    state[NEWS_LIST] = list
+  },
+  [SAVE_NEWS_DETAIL](state, detail) {
+    state[NEWS_DETAIL] = detail
+  }
+}
+
+export default new Vuex.Store({ state, getters, mutations })
