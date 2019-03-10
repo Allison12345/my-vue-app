5ca0692b6ebc128528f6fb85090b3aa7ed9e4227
Allison12345 <2562392373@qq.com>
2019-03-10 22:28:00 +0800

    paint-canvas

---
+++ src/pages/Paint.vue
@@ -0,0 +1,36 @@
+<template>
+  <canvas ref="list" @touchstart="onTouchStart" @touchmove="onTouchMove"></canvas>
+</template>
+<script>
+export default {
+  name: "paint",
+  data() {
+    return {
+      startX: 0,
+      startY: 0
+    };
+  },
+  methods: {
+    onTouchStart() {
+      var canvas = this.$refs.list;
+      var c = canvas.getContext("2d");
+      c.strokeStyle = "#f00";
+      c.beginPath();
+      this.startX = event.changedTouches[0].clientx;
+      this.startY = event.changedTouches[0].clientY;
+      c.moveTo(this.startX, this.startY);
+    },
+    onTouchMove() {}
+  }
+};
+</script>
+<style>
+body {
+  width: "100%";
+  height: "100%";
+}
+.list {
+  width: "100%";
+  height: "100%";
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,11 +3,13 @@ import Test from '../pages/Test'
 import Detail from '../pages/Detail'
 import State from '../pages/State'
 import Mystate from '../pages/Mystate'
+import Paint from '../pages/Paint'
 const routes = [
   { path: '/news', component: News },
   { path: '/test', component: Test },
   { path: '/detail', component: Detail },
   { path: '/state', component: State },
-  { path: '/mystate', component: Mystate }
+  { path: '/mystate', component: Mystate },
+  { path: '/paint', component: Paint }
 ]
 export default routes
